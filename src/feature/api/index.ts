5b3df9d5ad7bc0ca231a28c320ca04812adac186
root <root@Admin-PC.localdomain>
2023-12-22 10:48:28 +0700

    update api

--- src/feature/api/index.ts
+++ src/feature/api/index.ts
@@ -1,7 +1,8 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
+//http://localhost:4000
 export const baseQuery = fetchBaseQuery({
-  baseUrl: "http://localhost:4000",
+  baseUrl: "https://sang-store.onrender.com/",
   credentials: "include",
 });
 
